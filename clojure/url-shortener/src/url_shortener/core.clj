3349c36c8eb3a777c8b3690f34190ed18a816b23
Noyal Jose <junos.asteraceae@gmail.com>
2022-09-08 03:07:02 +0530

    second commit

--- clojure/url-shortener/src/url_shortener/core.clj
+++ clojure/url-shortener/src/url_shortener/core.clj
@@ -1,7 +1,15 @@
 (ns url-shortener.core
-  (:gen-class))
+  (:gen-class)
+  (:require [org.httpkit.server :as server]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
+(defn baby-small-app [req] ;(3)
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    ""})
+
+(defn -main ;(4)
+  "This is our app's entry point"
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8000"))] ;(5) 
+  (server/run-server #'baby-small-app {:port port})
+  (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
